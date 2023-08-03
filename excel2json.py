bf017092e51246cd66ba01d90aa37dd9547ca64c
Xuanmao Li <81699784+lmong11@users.noreply.github.com>
2023-08-03 22:59:02 +0800

    Update excel2json.py
    debug for datetime

--- excel2json.py
+++ excel2json.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import json
+from datetime import datetime
+
+def convert_datetime(obj):
+    if isinstance(obj, (pd.Timestamp, datetime)):
+        return obj.isoformat()
+    raise TypeError("Type not serializable")
 
 def convert_excel_to_json(excel_file_path, json_file_path):
     # Read the Excel file
@@ -28,13 +34,20 @@ def convert_excel_to_json(excel_file_path, json_file_path):
     columns_to_fill = ["Target Data Name", "Target Data Schema", "Target Data Description"]
     data_to_convert[columns_to_fill] = data_to_convert[columns_to_fill].ffill()
 
+    # Create a list to store the dictionaries
+    json_data = []
+
+    # Iterate through each row and append to the list
+    for _, row in data_to_convert.iterrows():
+        json_data.append(row.to_dict())
+
     # Open the JSON file for writing
     with open(json_file_path, 'w') as json_file:
-        # Iterate through each row and write to the JSON file with indentation
-        for _, row in data_to_convert.iterrows():
-            json_file.write(json.dumps(row.to_dict(), indent=4) + '\n')
+        json_data = [row.to_dict() for _, row in data_to_convert.iterrows()]
+        json_file.write(json.dumps(json_data, indent=4, default=convert_datetime))
 
     print(f"JSON file has been saved to {json_file_path}")
+
 # Path to the Excel file
 excel_file_path = 'D:/SQL/ChatGPT Benchmark Datasets.xlsx'
 
