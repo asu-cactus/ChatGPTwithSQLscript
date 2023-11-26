19261fb2c5e6367fcc2aa701296b0c0776993fad
ashar <ashar236@asu.edu>
2023-11-26 15:41:12 -0700

    Fixed Validation

--- auto_pipeline_join.py
+++ auto_pipeline_join.py
@@ -5,10 +5,12 @@ from datetime import datetime
 from util import (create_connection, execute_sql, print_experiment_settings,
                    log_experiment_success, log_experiment_failed)
 from join_util import convert_target_names,access_auto_pipeline_dataset,read_csv_target
-from gpt import chat_with_gpt
+from gpt import chat_with_gpt, gpt4_sql_script
 from join import validation
 import logging
-logging.basicConfig(filename='auto_pipeline_join.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+from io import StringIO
+
+logging.basicConfig(filename='auto_pipeline_join_valid_latest4.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def convert_datetime(obj):
     if isinstance(obj, (pd.Timestamp, datetime)):
@@ -26,7 +28,8 @@ def convert_excel_to_json(excel_file_path, json_file_path):
         "Target Data Schema",
         "Source Data Name",
         "Source Data Schema",
-        "Target Data Description"]
+        "Target Data Description",
+        "3 Samples of Source Data"]
 
     # Read the specified columns from the first sheet
     data_to_convert = pd.read_excel(xls, sheet_name='Sheet2', usecols=columns_to_include)
@@ -59,40 +62,45 @@ def create_sample_i(samples):
     return sample_i
 
 def generate_prompt_auto_pipeline(no_of_source_tables,source_names,target_name,source_data_schema,
-                                  target_data_schema,target_data_description,test_0_path,test_1_path,
+                                  target_data_schema,target_data_description,samples,test_0_path,test_1_path,
                                   sub_folder,template_option):
     target_name = target_name[0] 
     no_of_source_tables = no_of_source_tables
     source_data_schema = source_data_schema
     target_data_schema = target_data_schema[0]
     target_data_description = target_data_description[0]
-    if no_of_source_tables == 1:
-        prompt = f"""You are a SQL developer. Please generate a Postgres sql script to convert the {no_of_source_tables} source table to be consistent with the format of the target table {target_id}. 
-        First, you must create the source table with following {source_names} with only the given attributes: {source_data_schema}. 
-        Please delete the table before creating it if the table exists. 
-        Second, the table should have data from csv files at given path {test_0_path}.
-        Also treat empty value as NULL and You can use the HEADER option in the COPY command to skip the first row (which contains column names)
+    sample_i = create_sample_i(samples)
+    sample_0 = sample_i.get("sample_0")
+    sample_1 = sample_i.get("sample_1")
+
+    if no_of_source_tables == 1 and template_option == 4:
+        prompt = f"""
+        You are a SQL developer. Please generate a Postgres sql script to convert the {no_of_source_tables} source table to be consistent with the format of the target table {target_id}. 
+        First, you must create {no_of_source_tables} source table with following {source_names} with only the given attributes: {source_data_schema}. 
+        Please delete the table before creating it if the first table exists.
+        Source table samples are as follows {sample_0}.
+        Second, insert the entire csv file with headers from given paths {test_0_path} the {no_of_source_tables} source table respectively (treat empty value as NULL):
         Third, you must create a target table named {target_name} with only the given attributes: {target_data_schema}. 
         Please delete the table before creating it if the first table exists.
-        Finally, insert all rows from the source table into only one {target_name}, note that the selection clause in the insert statement should ignore attributes that are not needed.
         Hint-1: {target_data_description}
-        Also COPY the SQL result into a {sub_folder}{target_name}_result.csv" file.
+        Finally, insert all rows from the source table into only one {target_name}, note that the selection clause in the insert statement should ignore attributes that are not needed.
+        Also COPY the SQL result into a f"{sub_folder}{target_name}_result.csv" file. Use client-side facility such as psql's copy.
         Please don't remove the any table, because we need it for validation.
         Please quote the returned SQL script between "```sql\n" and "\n```".
         """
-    elif no_of_source_tables > 1:
+    elif no_of_source_tables == 2 and template_option == 4:
         prompt = f"""You are a SQL developer. Please generate a Postgres sql script to convert the {no_of_source_tables} source table to be consistent with the format of the target table {target_id}. 
         First, you must create the {no_of_source_tables} tables with following {source_names} with only the given attributes respectively: {source_data_schema}. 
-        Please delete the table before creating it if the first table exists. 
-        Second, these {no_of_source_tables} tables should have data from csv files at given path {test_0_path} and {test_1_path} respectively.
-        Also treat empty value as NULL and You can use the HEADER option in the COPY command to skip the first row (which contains column names)
+        Please delete the table before creating it if the first table exists.
+        First table samples are as follows {sample_0} and Second table samples are as follows {sample_1}.
+        Second, insert the entire csv file with headers from given paths {test_0_path} and {test_1_path} into the {no_of_source_tables} tables respectively (treat empty value as NULL):
         Third, you must create a target table named {target_name} with only the given attributes: {target_data_schema}. 
         Please delete the table before creating it if the first table exists.
-        Finally, insert all rows from the {no_of_source_tables} tables into only one {target_name}, note that the selection clause in the insert statement should ignore attributes that are not needed.
         Hint-1: {target_data_description}
-        Also COPY the SQL result into a {sub_folder}{target_name}_result.csv" file.
+        Finally, join all rows from the {no_of_source_tables} tables into only one {target_name}, note that the selection clause in the insert statement should ignore attributes that are not needed.
+        Also COPY the SQL result into a f"{sub_folder}{target_name}_result.csv" file. Use client-side facility such as psql's copy.
         Please don't remove the any table, because we need it for validation.
-        Please quote the returned SQL script between "```sql\n" and "\n```".
+        Please quote the returned SQL script between "```sql\n" and "\n```". 
         """
     else:
         print("choose different template option")
@@ -106,6 +114,7 @@ def gpt_auto_pipeline(json_file_path, target_data_name_to_find):
     source_data_schema = []
     target_data_schema = []
     target_data_description = []
+    samples = []
     for target_key, target_values in data_list.items():
         for target_value in target_values:
             if target_value.get("Target Data Name") == target_data_name_to_find:
@@ -114,7 +123,10 @@ def gpt_auto_pipeline(json_file_path, target_data_name_to_find):
                 source_data_schema.append(target_value.get("Source Data Schema"))
                 target_data_schema.append(target_value.get("Target Data Schema"))
                 target_data_description.append(target_value.get("Target Data Description"))
-    return target_data_names, source_data_names,source_data_schema, target_data_schema, target_data_description
+                samples.append(target_value.get("3 Samples of Source Data"))
+                #ground_truth_sql_result.append(target_value.get("Ground Truth SQL"))
+    return target_data_names, source_data_names,source_data_schema, target_data_schema, target_data_description,samples
+
 
 def main(*args):
     (json_file_path, template_option, target_id, max_target_id,length_id) = args
@@ -123,7 +135,8 @@ def main(*args):
     while target_id <= max_target_id:
         target_data_name_to_find = "Target" + str(length_id) + "_" + str(target_id)
         # Get JSON data for prompt
-        target_data_names, source_data_names,source_data_schema, target_data_schema, target_data_description = gpt_auto_pipeline(json_file_path,target_data_name_to_find)
+        target_data_names, source_data_names,source_data_schema, target_data_schema, target_data_description,samples = gpt_auto_pipeline(json_file_path,target_data_name_to_find)
+        no_of_source_tables = len(source_data_names)
         find_target_name_folder = convert_target_names(target_data_names[0])
         main_folder_name,sub_folder, test_0_path, test_1_path, target_path = access_auto_pipeline_dataset(find_target_name_folder) 
         logging.info(f"target_data_name, Source_data_names: {target_data_names[0]}, {source_data_names}")
@@ -141,25 +154,34 @@ def main(*args):
         # Run the experiment
         chatgpt_prompt = generate_prompt_auto_pipeline(no_of_source_tables,source_data_names,
                                                            target_data_names,source_data_schema, target_data_schema,
-                                                           target_data_description,test_0_path,test_1_path,sub_folder,template_option)
+                                                           target_data_description,samples,test_0_path,test_1_path,sub_folder,template_option)
   
         logging.info(f"final prompt: {chatgpt_prompt}")
-
-        #chatgpt_output = generate_prompt_auto_pipeline()
-        logging.info(f"Prompt for GPT Response:{chatgpt_prompt}")
-        gpt_output = chat_with_gpt(chatgpt_prompt)
+        #gpt_output = chat_with_gpt(chatgpt_prompt)
+        total_tokens=10000
+        gpt_output = gpt4_sql_script(chatgpt_prompt, total_tokens)
         logging.info(f"gold gpt sql: {gpt_output}")
         # Execute the SQL script on the specified table
         sql_result = execute_sql(conn, gpt_output)
+        sql_result_df = pd.DataFrame(sql_result)
         logging.info(f"SQL Result: {sql_result}")
+        logging.info(f"sql_result_df {sql_result_df}")
+        sql_result_df_sort = sql_result_df.sort_values(by=list(sql_result_df.columns))
+        logging.info(f"sql_result_df_sort {sql_result_df_sort}")
+        
         logging.info(f"target path: {target_path}")
         logging.info(f"{main_folder_name}{sub_folder}{target_data_name_to_find}_result.csv")
         if (validation_table_created == False):
             gold_target_csv = read_csv_target(target_path)
+            gold_target_csv_pd = pd.read_csv(target_path)
+            gold_target_csv_df = pd.DataFrame(gold_target_csv_pd)
+            logging.info(f"gold_target_csv_df {gold_target_csv_df}")
+            gold_target_csv_df_sort = gold_target_csv_df.sort_values(by=list(gold_target_csv_df.columns))
+            logging.info(f"gold_target_csv_df_sort {gold_target_csv_df_sort}")
             validation_table_created = True
         # Validate the ChatGPT generated SQL script
         logging.info(f"validation_table_created: {validation_table_created}")
-        case_accuracy, is_correct, similarity_scores, validation_error = validation(sql_result,gold_target_csv)
+        case_accuracy, is_correct, similarity_scores, validation_error = validation(sql_result_df_sort,gold_target_csv_df_sort)
         accuracy_list.append(case_accuracy)
         all_similarity_scores.append(similarity_scores)
         logging.info(f"is_correct and similarity_score: {is_correct} {similarity_scores}")
@@ -178,17 +200,19 @@ def main(*args):
 if __name__ == "__main__":
     # Path to the Excel file
     # excel_file_path = 'auto-pipeline-small.xlsx'
-    excel_file_path = 'auto-pipeline.xlsx'
+    excel_file_path = 'auto-pipeline-100.xlsx'
 
     # Path to save the JSON file
-    json_file_path = 'auto-pipeline.json'
+    json_file_path = 'auto-pipeline-100.json'
 
     # Call the function to perform the conversion
     convert_excel_to_json(excel_file_path, json_file_path)
-    template_option = 2 
+    template_option = 4 
     #length{length_id}_{target_id} is length1_2
     length_id = 1 
-    target_id, max_target_id = 1, 1
+    target_id, max_target_id = 17,17
     source_id, max_source_id = 0 , 2
     print_experiment_settings(template_option, target_id, max_target_id, source_id, max_source_id)
+    logging.info(f"*********** Starting template option and target_id: {template_option},{target_id}****************")
+    print(f"*Starting template option and target_id: {template_option},{target_id}")
     main(json_file_path, template_option, target_id, max_target_id,length_id)
